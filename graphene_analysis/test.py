58d9f46991360ab305921eb3c95531794265fd7d
Fabian Thiemann <ucapflt@ucl.ac.uk>
2021-01-10 22:56:53 +0000

    Update some io scripts and related tests

--- graphene_analysis/test.py
+++ graphene_analysis/test.py
@@ -1,76 +1,75 @@
+import ase
 import glob
 import os
-
-import ase
-from ase import io
-from ase.io import lammpsrun
-
 
 class UnableToFindFile(Exception):
     pass
 
-
-def get_path_to_file(directory_path, file_suffix, file_prefix=None):
+def get_path_to_file(directory_path, file_suffix,file_praefix=None):
     """
-    Return the path to file given directory, prefix and suffix.
+    Return the path to file given directory, praefix and suffix.
 
     Arguments:
         directory_path: The path to the directory containing the trajectory.
         file_suffix: The general suffix or type of the file.
-        file_prefix: The general name of the file.
+        file_praefix: The general name of the file.
 
     Returns:
         path_to_file: The path to the requested file.
     """
-    files_in_path = sorted(glob.glob(os.path.join(directory_path, f"*.{file_suffix}")))
+
+    files_in_path = glob.glob(os.path.join(directory_path, f"*.{file_suffix}"))
 
     if not files_in_path:
         raise UnableToFindFile(f"No {file_suffix} file found in path {directory_path}.")
 
     # If name for pdb file is given, we take that file.
-    if file_prefix:
-        return [file for file in files_in_path if file_prefix + f".{file_suffix}" in file][0]
+    if file_praefix:
+        return [
+            file for file in files_in_path if file_praefix + f".{file_suffix}" in file
+        ][0]
     # Otherwise, we take the first file given by glob.
     else:
         if len(files_in_path) > 1:
-            file_name_found = files_in_path[0].split("/")[-1]
+            file_name_found=files_in_path[0].split("/")[-1]
             print(
                 f"WARNING: More than one {file_suffix} file found. Proceeding with {file_name_found}."
             )
         return files_in_path[0]
 
 
+
 def get_ase_atoms_object(pdb_file_path):
     """
     Return an ase atoms object used for topology.
     This happens via the previously found pdb-file.
-    If this fails, the package will attempt to use
-    a lammpstrj file with the same name in the same
+    If this fails, the package will attempt to use 
+    a lammpstrj file with the same name in the same 
     directory.
 
     Arguments:
         pdb_file_path: The path to the pdb file.
 
     Returns:
-        ase_AO : The ase atoms object.
+        ase_AO : The ase atoms object. 
     """
 
-    try:
+    try: 
         # Try to use pdb file to generate atoms object
         return ase.io.read(pdb_file_path)
 
     except KeyError:
         print(
             "WARNING: Could not read pdb file with ASE, maybe you created it with VMD from a lammpstrj file."
-        )
+            )
 
         # Get directory path and name we look for
-        directory_path = "/".join([i for i in pdb_file_path.split("/")[0:-1]])
-        file_prefix = (pdb_file_path.split("/"))[-1].split(".")[0]
+        directory_path = "".join([i for i in pdb_file_path.split('/')[0:-1]])
+        file_praefix = (pdb_file_path.split('/'))[-1].split('.')[0] 
         print(f"Looking for lammpstrj files in {directory_path}")
 
         # Look for lammpstrj files in same directory
-        lammpstrj_file = get_path_to_file(directory_path, "lammpstrj", file_prefix)
+        lammpstrj_file = get_path_to_file(directory_path,file_praefix,"lammpstrj")
 
         print(f"Creating ASE atoms object for first frame of {lammpstrj_file}")
         ase_AO = ase.io.lammpsrun.read_lammps_dump(lammpstrj_file, index=0)
@@ -79,3 +78,8 @@ def get_ase_atoms_object(pdb_file_path):
         ase_AO.set_chemical_symbols(["C"] * ase_AO.get_global_number_of_atoms())
 
         return ase_AO
+
+
+
+
+
--- tests/test_io.py
+++ tests/test_io.py
@@ -1,24 +1,51 @@
-
+import pytest
 import sys
-sys.path.append('../')
+from unittest import mock
 
+sys.path.append("../")
 from graphene_analysis import io
 
 
-class TestGetPdfFileName:
+class TestGetPathToFile:
+    def test_returns_path_not_found(self):
+        path = "./files"
+        suffix = "random_suffix"
 
-	def test_returns_first_file(self):
-		path = './test_files'
+        with pytest.raises(io.UnableToFindFile):
+            io.get_path_to_file(path, suffix)
 
-		file_name = io.get_pdb_file_name(path)
+    def test_returns_first_file(self):
+        path = "./files"
+        suffix = "pdb"
 
-		assert file_name == './test_files/test2.pdb'
+        file_name = io.get_path_to_file(path, suffix)
+
+        assert file_name == "./files/graphene_1_divacancy.pdb"
+
+    def test_returns_requested_file(self):
+        path = "./files"
+        suffix = "pdb"
+        prefix = "graphene_1_divacancy"
+
+        file_name = io.get_path_to_file(path, suffix, prefix)
+
+        assert file_name == "./files/graphene_1_divacancy.pdb"
 
 
-	def test_returns_requested_file(self):
-		path = './test_files'
-		name = 'test1'
+class TestGetAseAtomsObject:
+    @mock.patch.object(io, "get_path_to_file")
+    def test_returns_ase_AO_from_pdb(self, get_path_to_file_mock):
+        path_to_pdb = "./files/graphene_20_monovacancy.pdb"
 
-		file_name = io.get_pdb_file_name(path, name)
+        ase_AO = io.get_ase_atoms_object(path_to_pdb)
 
-		assert file_name == './test_files/test1.pdb'
+        get_path_to_file_mock.assert_not_called()
+        assert ase_AO.get_global_number_of_atoms() == 7180
+
+    def test_returns_ase_AO_from_lammpstrj(self):
+        # This file is not readible by ase.
+        path_to_pdb = "./files/graphene_1_divacancy.pdb"
+
+        ase_AO = io.get_ase_atoms_object(path_to_pdb)
+
+        assert ase_AO.get_global_number_of_atoms() == 7198
