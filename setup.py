6f2ffd1048cf2bb6054ca2f9ddcf3ae0ac1d8798
flt17 <ucapflt@ucl.ac.uk>
2021-05-11 14:18:37 +0100

    Fixing typo in setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="confined-water-analysis",
+    name="graphene-analysis",
     version="0.1.0",
     url="https://tbd.com",
     author="Fabian Thiemann",
